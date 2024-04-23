75bc7c73157504b16a7e5ce3b5bd6d36bea7a0dd
Masgharoth <maksymilian.turski@gmail.com>
2024-04-24 01:18:55 +0200

    conf

--- init.lua
+++ init.lua
@@ -527,8 +527,7 @@ require('lazy').setup({
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format lua code
-        'prettier',
-        'prettierd',
+        'lua_ls',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
